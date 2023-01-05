2f56ac6629e83e9a15bf83e5be151d30767f8185
thomasduft <thomas.duft@gmail.com>
2023-01-05 18:18:43 +0000

    restructured project

--- src/smoky/Utils/JsonExtensions.cs
+++ src/smoky/Utils/JsonExtensions.cs
@@ -1,24 +1,14 @@
-using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace tomware.Smoky;
 
 internal static class JsonExtensions
 {
-  private static readonly JsonSerializerOptions _jsonOptions = new()
-  {
-    PropertyNameCaseInsensitive = true,
-    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-    WriteIndented = true
-  };
-
   public static T FromJson<T>(this string json)
   {
-    var result = JsonSerializer.Deserialize<T>(json, _jsonOptions);
+    var result = JsonConvert.DeserializeObject<T>(json);
     if (result is null) throw new InvalidDataException("Json string could not be deserialized");
 
     return result;
   }
-
-  public static string ToJson<T>(this T obj) =>
-      JsonSerializer.Serialize<T>(obj, _jsonOptions);
 }
