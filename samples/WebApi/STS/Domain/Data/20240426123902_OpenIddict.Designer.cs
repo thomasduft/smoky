30c591f29d019c7d32614010de61ed2159f374cd
thomasduft <thomas.duft@gmail.com>
2024-04-26 14:41:33 +0200

    Update package versions

--- samples/WebApi/STS/Domain/Data/20240426123902_OpenIddict.Designer.cs
+++ samples/WebApi/STS/Domain/Data/20240426123902_OpenIddict.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi;
 
@@ -10,9 +11,11 @@ using WebApi;
 namespace WebApi.STS.Domain.Data
 {
     [DbContext(typeof(STSDbContext))]
-    partial class STSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240426123902_OpenIddict")]
+    partial class OpenIddict
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
